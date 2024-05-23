83ae657d31d52a955914353227a8022419f36581
Alexander Egorov <egoroff@gmail.com>
2024-05-23 09:57:40 +0300

    feat: Subarray Sums I task added

---
+++ src/bin/subarray_sum.rs
@@ -0,0 +1,81 @@
+use std::io::{self, BufRead, BufReader};
+
+fn main() {
+    let reader = BufReader::new(io::stdin());
+    let result = solution(reader);
+    println!("{result}");
+}
+
+pub fn solution<B: BufRead>(mut r: B) -> i32 {
+    let lines = read_lines(&mut r, 1);
+    let parts: Vec<&str> = lines[0].split_whitespace().collect();
+    let n = parts[0].parse::<usize>().unwrap_or(0);
+    let x = parts[1].parse::<i32>().unwrap_or(0);
+
+    let lines = read_lines(&mut r, 1);
+    let parts: Vec<&str> = lines[0].split_whitespace().collect();
+
+    let nums: Vec<i32> = parts
+        .iter()
+        .take(n)
+        .map(|s| s.parse::<i32>().unwrap_or(0))
+        .collect();
+
+    let mut count = 0;
+
+    let mut r = 0;
+    let mut sum = 0;
+    for l in 0..nums.len() {
+        while r < nums.len() && sum < x {
+            if r > l {
+                sum += nums[r];
+            } else {
+                sum += nums[l];
+            }
+
+            r += 1;
+        }
+        if sum == x {
+            count += 1;
+        }
+        sum -= nums[l];
+    }
+
+    count
+}
+
+fn read_lines<B: BufRead>(src: &mut B, n: i32) -> Vec<String> {
+    let mut res = vec![];
+
+    for _ in 0..n {
+        let mut input = String::new();
+
+        match src.read_line(&mut input) {
+            Ok(_n) => {
+                res.push(input);
+            }
+
+            Err(error) => println!("error: {}", error),
+        }
+    }
+
+    res
+}
+#[cfg(test)]
+mod tests {
+    use std::cmp::Ordering;
+    use std::{
+        ffi::OsStr,
+        fs::{self, File},
+        path::Path,
+    };
+
+    use cses::run_test_suite;
+
+    use super::*;
+
+    #[test]
+    fn test_suite() {
+        run_test_suite!("/home/egr/Downloads/sumarray_sums_tests", solution);
+    }
+}
